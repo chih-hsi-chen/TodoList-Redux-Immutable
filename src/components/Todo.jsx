8d7865efc9a00aad15261fdf75b1a4c94b3f4cd0
chih-hsi-chen <chih.hsi.chen@gmail.com>
2020-04-29 10:25:09 +0800

    ready to use immutable state

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 import { toggleTodo, deleteTodo } from "../actions/actions";
 
-const Todo = ({ todo, toggleTodo, deleteTodo }) => (
-	<li
-		className="todo-item"
-		onClick={() => toggleTodo(todo.id) /** dispatches action to toggle todo */}
-	>
-		{todo && todo.completed ? "👌" : "👋"}{" "}
-		<span
-			className={`todo-item__text ${todo && todo.completed && "todo-item__text--completed"}`}
+const Todo = ({ todo, toggleTodo, deleteTodo }) => {
+	console.log(`${todo.id} is rendering`);
+	return (
+		<li
+			className="todo-item"
+			onClick={() => toggleTodo(todo.id) /** dispatches action to toggle todo */}
 		>
-			{todo.content}
-		</span>
-		<button
-			onClick = {(e) => {
-				e.stopPropagation();
-				deleteTodo(todo.id);
-			}}
-		>delete</button>
-	</li>
-);
+			{todo && todo.completed ? "👌" : "👋"}{" "}
+			<span
+				className={`todo-item__text ${todo && todo.completed && "todo-item__text--completed"}`}
+			>
+				{todo.content}
+			</span>
+			<button
+				onClick={(e) => {
+					e.stopPropagation();
+					deleteTodo(todo.id);
+				}}
+			>delete</button>
+		</li>
+	);
+};
 
 export default connect(
 	null,
